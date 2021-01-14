7627910f849c6a52fd0450df78b9a42e7f3a5b7e
남민기 <nammingi@nammingiui-MacBookPro.local>
2021-01-15 05:44:06 +0900

    0115 0544

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import "./Movie.css";
 
-function Movie({ id, year, title, summary, poster, genres }) {
+function Movie({ key, id, year, title, summary, poster, genres }) {
   return (
     <div className="movie">
-      <img src={poster} alt={title} title={title} />
+      <img className="poster" src={poster} alt={title} title={title} />
       <div className="movie__data">
         <h3 className="movie__title">{title}</h3>
         <h5 className="movie__year">{year}</h5>
@@ -17,7 +18,20 @@ function Movie({ id, year, title, summary, poster, genres }) {
           ))}
         </ul>
         <p className="movie__summary">{summary.slice(0, 180)}...</p>
-        <p className="viewmore">[View more]</p>
+        <Link
+          to={{
+            pathname: `/movie/${id}`,
+            state: {
+              year,
+              title,
+              summary,
+              poster,
+              genres,
+            },
+          }}
+        >
+          <p className="viewmore">[View more]</p>
+        </Link>
       </div>
     </div>
   );
@@ -27,7 +41,7 @@ Movie.propTypes = {
   year: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
-  poster: PropTypes.string,
+  poster: PropTypes.string.isRequired,
   genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
---
+++ src/routes/Detail.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Detail extends React.Component {
+  componentDidMount() {
+    const { location, history } = this.props;
+    if (location.state === undefined) {
+      history.push("/");
+    }
+  }
+  render() {
+    return <span>details</span>;
+  }
+}
+
+export default Detail;
